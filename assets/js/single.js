31f3ab307cf94c4a91a8743c8d766bc8631ff494
tracybrtn <tracynburton@gmail.com>
2022-10-07 12:18:01 -0500

    warning function working

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     // format the github api url
@@ -12,6 +13,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 displayIssues(data);
 
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } 
         else {
@@ -58,9 +63,20 @@ var displayIssues = function(issues) {
 
          // append to the dom
         issueContainerEl.appendChild(issueEl);
-
     }
+};
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+     // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
