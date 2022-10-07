692d73dcdaf7ae6d50a0830b6519cc772fea6d50
tracybrtn <tracynburton@gmail.com>
2022-10-07 13:04:25 -0500

    list of featured repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
+var languageButtonsEl = document.querySelector('#language-buttons');
 var repoSearchTerm = document.querySelector('#repo-search-term');
 
 var formSubmitHandler = function(event) {
@@ -20,6 +21,18 @@ var formSubmitHandler = function(event) {
     alert('Please enter a GitHub username');
   }
 };
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+  }
+}
 
 var getUserRepos = function(user) {
   // format the github api url
@@ -43,6 +56,20 @@ var getUserRepos = function(user) {
       alert('Unable to connect to GitHub');
     });
 };
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+}
 
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
@@ -92,3 +119,4 @@ var displayRepos = function(repos, searchTerm) {
 
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
